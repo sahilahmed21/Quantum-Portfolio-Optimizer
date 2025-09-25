07692354aa3a391ae54c7fe9e2e1b3b0ef20f5ee
sahilahmed21 <sahilahmedpbuh@gmail.com>
2025-09-25 16:28:35 +0530

    better frontend3

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,6 +9,8 @@ import { HoverEffect } from "@/components/ui/card-hover-effect";
 import { AnimatePresence, motion } from "motion/react";
 import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 import { quantumFeatures } from "@/components/quantumfeatures";
+import { Icon } from "@/components/icons";
+
 
 export default function Home() {
   return (
@@ -257,18 +259,18 @@ const ResultsIcon = () => (
   </svg>
 );
 
-export const Icon = ({ className, ...rest }: any) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className={className}
-      {...rest}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-    </svg>
-  );
-};
+// export const Icon = ({ className, ...rest }: any) => {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       strokeWidth="1.5"
+//       stroke="currentColor"
+//       className={className}
+//       {...rest}
+//     >
+//       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+//     </svg>
+//   );
+// };
